f0a29bcb76245ebd771b875e94a8965bcec3edee
shahidul shohid <shahidulislamshohid7gmail.com>
2025-01-19 15:45:41 +0600

    get method implement is done for getting one specific data for view booked session

--- index.js
+++ index.js
@@ -179,6 +179,14 @@ async function run() {
       res.send(result)
     })
 
+    //get method for getting one specific data for view booked session
+    app.get('/bookedSession/:id', async(req, res) => {
+      const id = req.params.id 
+      const query = {_id: new ObjectId(id)}
+      const result = await BookedSessionCollection.findOne(query)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
