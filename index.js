0217b56ee6f9079963db032707541865d8fe2122
shahidul shohid <shahidulislamshohid7gmail.com>
2025-01-20 17:43:27 +0600

    get method is done for getting one data for update page

--- index.js
+++ index.js
@@ -211,6 +211,22 @@ async function run() {
       res.send(result)
     })
 
+    //get method for getting one specific data for update page(student)
+    app.get('/notes/:id', async(req, res)=> {
+      const id = req.params.id 
+      const query = {_id: new ObjectId(id)}
+      const result = await createNotesCollection.findOne(query)
+      res.send(result)
+    })
+    
+    // delete method for deleting note form manage personal notes page(student)
+    app.delete('/notes/:id', async(req, res) => {
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await createNotesCollection.deleteOne(query)
+      res.send(result)
+    })
+
     //get method for getting all notes by email for manage personal notes page(student)
     app.get('/allNotes/:email', async(req, res) => {
       const email = req.params.email 
